370f60bad63e1d7219906e34e5f6ec6545a10f92
Arthur99Silva <arthurassilva99@gmail.com>
2024-04-29 14:25:35 -0300

    Update YoloBasics.py

--- RunningYolo/YoloBasics.py
+++ RunningYolo/YoloBasics.py
@@ -3,6 +3,6 @@ import cv2
 
 
 model = YOLO('../Yolo-Weights/yolov8l.pt')
-results = model("C:/Users/Arthur/Documents/YOLO_Learning/RunningYolo/Images/street.jpg", show=True)
+results = model("C:/Users/Arthur/Documents/YOLO_Learning/YOLO/RunningYolo/Images/street1.png", show=True)
 cv2.waitKey(0)
 
